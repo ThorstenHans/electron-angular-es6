0e62afa6b0bfba6170cafbcef219a015bc4ee64e
Thorsten Hans <thorsten.hans@gmail.com>
2015-05-05 23:20:47 +0200

    added package tasks for gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,32 @@
 var gulp = require('gulp'),
   babel = require('gulp-babel'),
   run = require('gulp-run'),
-  rename = require('gulp-rename');
+  rename = require('gulp-rename'),
+  asar  = require('gulp-asar'),
+  clean = require('gulp-clean');
 
 gulp.task('transpile-app', function() {
   return gulp.src('app/index.es6.js')
     .pipe(babel())
     .pipe(rename('index.js'))
     .pipe(gulp.dest('app'));
+});
+
+
+gulp.task('clean', function(){
+    return gulp.src('package', {read: false})
+        .pipe(clean({force: true}));
+});
+
+gulp.task('copy-app', ['clean'], function(){
+    return gulp.src(['app/**/*', 'browser/**/*', 'package.json'], {base: '.'})
+        .pipe(gulp.dest('package'));
+});
+
+gulp.task('package', ['copy-app'], function(){
+    return gulp.src('package/**/*')
+        .pipe(asar('app.asar'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('run', ['default'], function() {
